d6674234f4c22ad7242460a9d6bb54ef69b682e2
NSIC Howrah <nsic.howrah2000@gmail.com>
2026-02-21 12:14:13 +0530

    Delete mongo.js

--- mongo.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectMongo = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log("MongoDB Connected");
-};
-
-module.exports = connectMongo;
